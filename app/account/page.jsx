1f642b5999de0d0c99b78f41d7f46754d16c6d76
aaliyah-sm <aaliyah.smallwood@hotmail.com>
2025-11-05 18:08:02 -0800

    Add account page layout and styling with form inputs

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import Header from '../components/header/Header.jsx'
-export default function page() {
+import "./page.css";
+import Header from "../components/header/Header.jsx";
+
+
+export default function Page() {
   return (
     <>
       <Header />
-      <h1>Account Page</h1>
+      <div className="hero-form">
+        <div className="text-content-title">
+          <p className="text-7">Edit Account Details</p>
+          <p className="text-9">How others see your account:</p>
+        </div>
+
+        <div className="form-contact">
+          <div className="input-field">
+            <p className="text-3">Name</p>
+          <input id="firstname" firstname="firstname" className="input" placeholder="Your First Name" />
+          
+          </div>
+
+          <div className="input-field">
+            <p className="text-5">Last Name</p>
+           <input id="lastname" lastname="lastname" className="input" placeholder="Your Last Name"/>
+          </div>
+
+          <div className="input-field">
+            <p className="text-7">Email</p>
+            <input id="email" email="email" className="input" placeholder="Your Email"/>
+          </div>
+
+          <div className="textarea-field">
+            <p className="text-9">About Me</p>
+            <input id="aboutme" aboutme="aboutme" className="input" placeholder="Describe Yourself"/>
+          </div>
+
+          <div className="button-group">
+            <button className="button">
+              <p className="text-1-1">Submit</p>
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
