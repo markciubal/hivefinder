9fee7907cb69497423bb2413dca21ad15f4d241a
Annyus Chandler <annyuschandler20@gmail.com>
2025-11-30 22:01:49 -0800

    Update page.jsx

--- app/clubpinUser/page.jsx
+++ app/clubpinUser/page.jsx
@@ -1,9 +1,131 @@
-import React from 'react'
+const ClubPinboardUserPage = () => {
+  return <div>Club Pinboard</div>;
+};
 
-function page() {
+const UserView = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="club-pinboard-page---user-view">
+      <div className="group-9">
+        <div className="club-name">
+          <p className="text-1">Club Name</p>
+        </div>
+        <div className="navigation">
+          <div className="items">
+            <p className="text-2">Home</p>
+            <p className="text-3">Contact</p>
+            <p className="text-4">Club</p>
+            <p className="text-5">Friend</p>
+            <div className="frame-1-3-2-1-3-1-7-4-6-8">
+              <div className="frame-1-3-2-1-3-1-7-4-6-7">
+                <div className="frame-1-3-2-1-3-1-7-4-6-6">
+                  <p className="text-6">Account</p>
+                  <img src="" />
+                </div>
+              </div>
+            </div>
+            <button className="button">
+              <p className="text-8">🙂</p>
+            </button>
+          </div>
+          <img src="" />
+        </div>
+        <div className="group-4">
+          <p className="text-1-0">First Last - Title</p>
+          <p className="text-1-1">First Last - Title</p>
+          <p className="text-1-2">First Last - Title</p>
+          <p className="text-1-3">Officers:</p>
+        </div>
+        <div className="group-3">
+          <p className="text-1-4">clubemail@college.edu</p>
+          <p className="text-1-5">Email:</p>
+          <p className="text-1-6">Website:</p>
+          <p className="text-1-7">www.clubwebsite.net</p>
+        </div>
+        <div className="divider" />
+        <div className="navigation-/-footer">
+          <p className="text-1-8">Location</p>
+          <p className="text-1-9">CSU Sacramento
+6000 J St
+Sacramento, CA 95819</p>
+          <p className="text-2-0">Need Help? Contact Us.
 
-export default page
+hivefindersupport@csus.edu</p>
+          <p className="text-2-1">HiveFinder</p>
+          <div className="divider" />
+          <div className="frame-1-3-2-1-3-1-7-4-5-8" />
+        </div>
+        <div className="group-5">
+          <img src="" />
+          <div className="postit" />
+          <div className="postit" />
+          <img src="" />
+          <img src="" />
+          <p className="text-2-5">club meeting details</p>
+          <p className="text-2-6">important updates</p>
+          <img src="" />
+          <img src="" />
+          <p className="text-2-9">Club Description: Allied Students for Justice (ASFJ) is an inclusive platform designed to  empower students and promote social justice at California State  University, Sacramento (CSUS). ASFJ's platform aims to inspire, educate, and mobilize individuals towards creating a more equitable and  inclusive society. By fostering dialogue, organizing events, and  providing resources, ASFJ encourages students to become active advocates for social justice on and off campus.</p>
+        </div>
+        <div className="divider" />
+        <div className="upcoming-events-header">
+          <p className="text-3-3">Where to Find Us!</p>
+        </div>
+        <img src="" />
+        <div className="group-7">
+          <div className="component-5">
+            <div className="event">
+              <div className="rectangle-6" />
+            </div>
+            <div className="event">
+              <div className="rectangle-6" />
+            </div>
+            <p className="text-3-5">Date & Time</p>
+            <p className="text-3-6">Club Name</p>
+            <p className="text-3-7">Event Title</p>
+            <p className="text-3-8">Location</p>
+            <img src="" />
+          </div>
+          <div className="component-6">
+            <div className="event">
+              <div className="rectangle-6" />
+            </div>
+            <div className="event">
+              <div className="rectangle-6" />
+            </div>
+            <p className="text-4-0">Date & Time</p>
+            <p className="text-4-1">Club Name</p>
+            <p className="text-4-2">Event Title</p>
+            <p className="text-4-3">Location</p>
+            <img src="" />
+          </div>
+          <div className="component-7">
+            <div className="event">
+              <div className="rectangle-6" />
+            </div>
+            <div className="event">
+              <div className="rectangle-6" />
+            </div>
+            <p className="text-4-5">Date & Time</p>
+            <p className="text-4-6">Club Name</p>
+            <p className="text-4-7">Event Title</p>
+            <p className="text-4-8">Location</p>
+            <img src="" />
+          </div>
+          <div className="component-8">
+            <div className="event">
+              <div className="rectangle-6" />
+            </div>
+            <div className="event">
+              <div className="rectangle-6" />
+            </div>
+            <p className="text-5-0">Date & Time</p>
+            <p className="text-5-1">Club Name</p>
+            <p className="text-5-2">Event Title</p>
+            <p className="text-5-3">Location</p>
+            <img src="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
