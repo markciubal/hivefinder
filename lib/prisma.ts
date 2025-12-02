19014e866b974e8ac90f1abf752f19db1bb1c5a3
Mark Ciubal <mark.ciubal@gmail.com>
2025-12-02 11:45:37 -0800

    Fixed prisma declaration error.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
+// Extend the global object to include the 'prisma' property
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
 const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export { prisma };
