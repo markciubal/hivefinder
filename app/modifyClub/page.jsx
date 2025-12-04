15763276b1c0f256b86e75fdb521d9589052a501
aaliyah-sm <aaliyah.smallwood@hotmail.com>
2025-12-03 16:00:03 -0800

    Added create event page, modify club page, and view event page

--- app/modifyClub/page.jsx
+++ app/modifyClub/page.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import "./page.css";
+import Header from "../components/header/Header.jsx";
 
-function page() {
+
+export default function Page() {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <Header />
+       <div className="modify-container">
+      <div className="modify-card">
 
-export default page
+        <h1 className="modify-title">Modify Club Information</h1>
+
+        {/* Email */}
+        <label className="input-label">Email</label>
+        <input className="input-field" placeholder="Enter Club Email" />
+
+        {/* Website */}
+        <label className="input-label">Website</label>
+        <input className="input-field" placeholder="Enter Club Website" />
+
+        {/* Description */}
+        <label className="input-label">Description</label>
+        <textarea
+          className="textarea-field"
+          placeholder="Describe club or add mission statement"
+        />
+
+        {/* Event Tags */}
+        <label className="input-label">Event Tags</label>
+        <input
+          className="input-field"
+          placeholder="Tag the event to connect with users with similar interests"
+        />
+
+        <div className="tags-row">
+          <div className="tag-chip">Fishing</div>
+          <div className="tag-chip">Camping</div>
+          <div className="tag-chip">Off-roading</div>
+        </div>
+
+        <button className="submit-btn">Submit</button>
+      </div>
+    </div>
+    </>
+  );
+}
